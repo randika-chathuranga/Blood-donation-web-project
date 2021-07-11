907a20851a2a7f764321d5d4896c7072029b98ff
ThiwankaShan <shane.thiwanka@gmail.com>
2021-07-11 11:29:57 +0530

    added update method to model

--- index.php
+++ index.php
@@ -5,13 +5,4 @@ include("private/Path.php");
 
 Server::execute();
 
-
-
-// Model,Connection Test
-require("private/models/User.php");
-$user = new User(array('fullName'=>"shan",'nic'=>"954871254v",'gender'=>"male",'dateOfBirth'=>date('1995-09-02'),'bloodGroup'=>"O+",'bodyWeight'=>"17kg",'adress'=>"some where in piliyandala",'email'=>'test@gmail.com','password'=>'somehashcode'));
-$user->save();
-//$user->delete();
-//User::fetch("nic",132);
-
 ?>
--- private/controllers/UserController.php
+++ private/controllers/UserController.php
@@ -1,11 +1,37 @@
 <?php
 
+require("private/models/User.php");
 
 class UserController
 {
     public static function home(){
+
+        
         $root = realpath($_SERVER["DOCUMENT_ROOT"]);
         include "$root/private/views/home.html";
+
+        //add user
+        //$user = new User(array('fullName'=>'shan','nic'=>'954871254v','gender'=>"male",'dateOfBirth'=>date('1995-09-02'),'bloodGroup'=>"O+",'bodyWeight'=>17,'adress'=>"some where in piliyandala",'email'=>'test@gmail.com','password'=>'somehashcode'));
+        //$user->save();
+
+        //delete user
+        //$user = new User(array('fullName'=>'shan','nic'=>'954871254v','gender'=>"male",'dateOfBirth'=>date('1995-09-02'),'bloodGroup'=>"O+",'bodyWeight'=>17,'adress'=>"some where in piliyandala",'email'=>'test@gmail.com','password'=>'somehashcode'));
+        //$user->delete();
+
+        //get user
+        //$results = User::fetch("nic",'954871254v');
+
+        //foreach($results as $result ){
+        //    echo($result->fullName);
+        //}
+
+        // update user
+        //$results = User::fetch("nic",'954871254v');
+
+        //$results[0]->fullName = "Thiwanka";
+
+        //$results[0]->update();
+        
     }
 
     public static function about(){
