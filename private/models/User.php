f5f89bb0a1f7aecc7e7b13ac6b9e47ed1b614492
ThiwankaShan <shane.thiwanka@gmail.com>
2021-07-11 12:59:19 +0530

    added create table method for user model, added deletebyValue method, created a UnitTests class

--- private/models/User.php
+++ private/models/User.php
@@ -11,12 +11,12 @@ class User extends Model{
     public $dateOfBirth;
     public $bloodGroup;
     public $bodyWeight;
-    public $adress;
+    public $address;
     public $email;
     public $password;
 
     function __construct($data){
-        // data array (fullName => value,nic => value,genderdateOfBirth => value,bloodGroup => value,bodyWeight => value,adress => value,email => value,password => value)
+        // data array (fullName => value,nic => value,genderdateOfBirth => value,bloodGroup => value,bodyWeight => value,address => value,email => value,password => value)
         
         $this->fullName = $data['fullName'];
         $this->nic = $data['nic'];
@@ -25,9 +25,24 @@ class User extends Model{
         $this->dateOfBirth = $data['dateOfBirth'];
         $this->bloodGroup = $data['bloodGroup'];
         $this->bodyWeight = $data['bodyWeight'];
-        $this->adress = $data['adress'];
+        $this->address = $data['address'];
         $this->email = $data['email'];
         $this->password = $data['password'];
+    }
+
+    public static function createTable(){
+        $sql = "CREATE TABLE " . static::$tableName ." (
+            fullName VARCHAR(30) NOT NULL,
+            nic VARCHAR(30) PRIMARY KEY NOT NULL,
+            gender VARCHAR(30) NOT NULL,
+            dateOfBirth DATE NOT NULL,
+            bloodGroup VARCHAR(20),
+            bodyWeight INT(20),
+            address VARCHAR(30),
+            email VARCHAR(50),
+            password VARCHAR(200) )";
+        
+        Connection::execute($sql);
     }
 }
 
