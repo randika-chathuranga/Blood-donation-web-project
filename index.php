8c7a2bd358b14ccf967cc4268af17f172755ed69
ThiwankaShan <shane.thiwanka@gmail.com>
2021-07-11 10:22:57 +0530

    fixed db column order issue

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ Server::execute();
 
 // Model,Connection Test
 require("private/models/User.php");
-$user = new User("shan","Thiwanka","s@gmail.com",132);
-//$user->save();
+$user = new User(array('fullName'=>"shan",'nic'=>"954871254v",'gender'=>"male",'dateOfBirth'=>date('1995-09-02'),'bloodGroup'=>"O+",'bodyWeight'=>"17kg",'adress'=>"some where in piliyandala",'email'=>'test@gmail.com','password'=>'somehashcode'));
+$user->save();
 //$user->delete();
-User::fetch("nic",132);
+//User::fetch("nic",132);
 
 ?>
